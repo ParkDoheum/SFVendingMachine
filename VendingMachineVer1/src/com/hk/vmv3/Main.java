bd87bc9995acb153527590695d252ecf15c381a8
USER25 <USER25@USER14-PC>
2019-10-28 17:00:43 +0900

    완료!

--- VendingMachineVer1/src/com/hk/vmv3/Main.java
+++ VendingMachineVer1/src/com/hk/vmv3/Main.java
@@ -7,37 +7,29 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		VendingMachine vm = new VendingMachine();
-		
+
 		System.out.print("이름을 입력해 주세요: ");
 		String name = scan.next();
-		
+
 		System.out.print("금액을 입력해 주세요: ");
 		int money = scan.nextInt();
-		
-		//Customer 객체화
+
+		// Customer 객체화
 		Customer cust = new Customer(name, money);
-		System.out.println(cust);
-		
-		vm.showDrinks();
-		
-		System.out.print("번호를 입력해 주세요: ");
-		int idx = scan.nextInt();
-		
-		vm.sales(idx, cust);
-		
+
+		while (true) {
+			vm.showDrinks();
+			System.out.print("구매할 번호를 입력해 주세요 (종료:0): ");
+			int idx = scan.nextInt();
+			if(idx == 0) {
+				break;
+			}
+			vm.sales(idx, cust);
+			System.out.println(cust);
+		}
 		System.out.println("--------------");
-		vm.showDrinks();
 		cust.showState();
-		
 		scan.close();
 	}
 
 }
-
-
-
-
-
-
-
-
